62aac5d963e1de42c9c5aae80ab0b5e405c9baeb
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-07-04 02:55:13 +0200

    github pages trial

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,39 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
+	kit: {
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs",
+		}),
+		paths: {
+			base: dev ? "" : "/TSF-Intern/",
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: "#svelte",
 
-    vite: {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: '@use "src/variables.scss" as *;',
-          },
-        },
-      },
-    },
-  },
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;',
+					},
+				},
+			},
+		},
+	},
 
-  preprocess: [
-    preprocess({
-      scss: {
-        prependData: '@use "src/variables.scss" as *;',
-      },
-    }),
-  ],
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;',
+			},
+		}),
+	],
 };
 
 export default config;
