99844bc9c7815c4e2e9d8fa39f61d61da6623f4b
Adel <56457765+aadelafattah@users.noreply.github.com>
2022-07-04 03:47:44 +0200

    netlify trial

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,30 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
+	kit: {
+		adapter: adapter({
+			edge: true,
+		}),
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;',
+					},
+				},
+			},
+		},
+	},
 
-    vite: {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: '@use "src/variables.scss" as *;',
-          },
-        },
-      },
-    },
-  },
-
-  preprocess: [
-    preprocess({
-      scss: {
-        prependData: '@use "src/variables.scss" as *;',
-      },
-    }),
-  ],
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;',
+			},
+		}),
+	],
 };
 
 export default config;
